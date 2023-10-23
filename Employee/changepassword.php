c4ee490f52eddbb8e7a805f02f3c9a56ad12eab9
VISHAL V NAIR <vishalvnair124@gmail.com>
2023-10-23 19:35:48 +0530

    checkbox showing error

--- Employee/changepassword.php
+++ Employee/changepassword.php
@@ -7,6 +7,7 @@
             <hr>
             <label for="cur-password">Current Password : </label>
             <input type="password" id="cur-password" placeholder="Password" name="cur-password" required>
+            <input type="checkbox" name="" id="">
             <br>
             <label id="cur-passwordinvalid" style="color: red;visibility: hidden;"></label>
             <br>
@@ -15,11 +16,13 @@
             <label for="new-password">New Password&ensp;&ensp;&ensp; : </label>
             <input type="password" id="new-password" placeholder="Password"
                 name="new-password" required>
+                <input type="checkbox" name="" id="">
             <br>
             <label id="new-passwordinvalid" style="color: red;visibility: hidden;"></label>
             <br>
             <label for="con-password">Confirm Password : </label>
             <input type="password" id="con-password" placeholder="Password" name="password" required>
+            <input type="checkbox" name="" id="">
             <br>
             <label id="con-passwordinvalid" style="color: red;visibility: hidden;"></label>
             <br>
