5e24876b5172b5ee4ded4e70d6e6d94cb15ba048
Time <abhinm24@gmail.com>
2023-10-27 22:32:06 +0530

    Employee page updated

--- Employee/changepassword.php
+++ Employee/changepassword.php
@@ -8,21 +8,21 @@
             <input type="password" id="cur-password" placeholder="Password" name="cur-password" required>
             <input onclick="typechange('cur-password')" type="checkbox" name="" id="">
         </div>
-        <label id="cur-passwordinvalid" style="color: red;visibility: hidden;"></label>
+        <label id="cur-passwordinvalid" style="color: red;visibility: hidden;">Not same</label>
         <div class="passchange">
             <label for="new-password">New Password&ensp;&ensp;&ensp; : </label>
             <input type="password" id="new-password" placeholder="Password" name="new-password" required>
             <input onclick="typechange('new-password')" type="checkbox" name="" id="">
         </div>
-        <label id="new-passwordinvalid" style="color: red;visibility: hidden;"></label>
+        <label id="new-passwordinvalid" style="color: red;visibility: hidden;">Not same</label>
         <div class="passchange">
             <label for="con-password">Confirm Password : </label>
             <input type="password" id="con-password" placeholder="Password" name="password" required>
             <input onclick="typechange('con-password')" type="checkbox" name="" id="">
         </div>
-        <label id="con-passwordinvalid" style="color: red;visibility: hidden;"></label>
+        <label id="con-passwordinvalid" style="color: red;visibility: hidden;">Not same</label>
         <div class="passchange">
-            <label id="same-password" style="color: red;visibility: hidden;">Two password should be same</label>
+            <label id="same-password" style="color: red; width:auto;visibility: hidden;">Two password should be same</label>
         </div>
         <div class="passchange">
             <div></div>
