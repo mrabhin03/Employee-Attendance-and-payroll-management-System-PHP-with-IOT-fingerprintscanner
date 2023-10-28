9b639b2d2b7bcf0b4f25f359ad42e3aaa63db3a4
VISHAL V NAIR <vishalvnair124@gmail.com>
2023-10-28 12:18:40 +0530

    Email sending code added

--- Employee/changepassword.php
+++ Employee/changepassword.php
@@ -7,7 +7,6 @@
             <hr>
             <label for="cur-password">Current Password : </label>
             <input type="password" id="cur-password" placeholder="Password" name="cur-password" required>
-            <input type="checkbox" name="" id="">
             <br>
             <label id="cur-passwordinvalid" style="color: red;visibility: hidden;"></label>
             <br>
@@ -16,13 +15,11 @@
             <label for="new-password">New Password&ensp;&ensp;&ensp; : </label>
             <input type="password" id="new-password" placeholder="Password"
                 name="new-password" required>
-                <input type="checkbox" name="" id="">
             <br>
             <label id="new-passwordinvalid" style="color: red;visibility: hidden;"></label>
             <br>
             <label for="con-password">Confirm Password : </label>
             <input type="password" id="con-password" placeholder="Password" name="password" required>
-            <input type="checkbox" name="" id="">
             <br>
             <label id="con-passwordinvalid" style="color: red;visibility: hidden;"></label>
             <br>
