806381134bc8171d50467b8b1f93f9961d31c10e
Time <abhinm24@gmail.com>
2023-12-05 19:51:15 +0530

    The Bonus Bug fixed

--- Admin/Payrolls.php
+++ Admin/Payrolls.php
@@ -9,6 +9,7 @@ include 'session_check.php';
     $Year = date('Y');
     $month = date('m');
   }
+  unset($_SESSION['cat']);
   $monthco = array(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
   ?>
 <div class="Payrolls">
