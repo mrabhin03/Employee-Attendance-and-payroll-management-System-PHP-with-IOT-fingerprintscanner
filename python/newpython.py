49f2a5a83384a575e02c215ee3a00cf7278e04ea
Time <abhinm24@gmail.com>
2024-01-05 12:12:12 +0530

    Improved

--- python/newpython.py
+++ python/newpython.py
@@ -20,12 +20,19 @@ while True:
         flag = 1
     elif value == '#':
         flag = 0
-        print(data)
-        raop= data.split(",")
+        adp = data
+        sql = "SELECT log_status FROM emp_logs WHERE Rf_id=%s AND log_id=(SELECT MAX(log_id) FROM emp_logs WHERE Rf_id=%s)"  # Remove single quotes around %s
+        mycursor.execute(sql, (adp,adp))  # Pass adp as a tuple to execute()
+        result = mycursor.fetchone()[0]
         
+        if result=="IN":
+            status="OUT"
+        else:
+            status="IN"
         sql = "INSERT INTO emp_logs (Rf_id,Log_status) VALUES (%s, %s)"
-        val = (raop[0], raop[1])
+        val = (adp, status)
         mycursor.execute(sql, val)
+        print("Your ID : ",adp," status : ", status)
         mydb.commit()
         data = ""
     if flag == 1:
